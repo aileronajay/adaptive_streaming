ef81eb55ae712da851c18c4922746a131c09f5db
Ajay Saxena <aileronajay@gmail.com>
2017-05-12 20:08:42 -0400

    Server, client and scripts

---
+++ testscripts/stable.sh
@@ -0,0 +1,108 @@
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 1000"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
---
+++ testscripts/stair_case.sh
@@ -0,0 +1,108 @@
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 100"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 100"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 100"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 200"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 200"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 200"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 300"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 300"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 300"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 400"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 400"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 400"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 500"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 500"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 500"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 400"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 400"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 400"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 300"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 300"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 300"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 200"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 200"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 200"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 100"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 100"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
+echo `date` +"%T";
+{ echo "auth ZwqeYMJ9+XMxM/X5"; sleep 1; echo "network speed 100"; echo "network status";} | telnet localhost 5554 ;
+sleep 10;
+
